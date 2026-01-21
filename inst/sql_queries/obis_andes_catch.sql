1ae460366c677295f23434ae7ac740c0bf860ab7
David Sean <david.sean@gmail.com>
2026-01-21 09:35:15 -0500

    add distinct, we do not want one entry per basket, but per catch

--- inst/sql_queries/obis_andes_catch.sql
+++ inst/sql_queries/obis_andes_catch.sql
@@ -1,5 +1,5 @@
 /* The OBIS occurrence Table is mostly constructed from this result based off of ANDES Catches*/
-SELECT 
+SELECT DISTINCT
     shared_models_referencecatch.scientific_name AS scientific_name,
     shared_models_referencecatch.aphia_id AS aphia_id,
     shared_models_sample.sample_number AS sample_number,
@@ -11,7 +11,7 @@ LEFT JOIN shared_models_referencecatch
 LEFT JOIN shared_models_sample 
 	ON shared_models_sample.id=shared_models_catch.sample_id
 LEFT JOIN shared_models_basket
-ON shared_models_basket.catch_id = shared_models_catch.id
+    ON shared_models_basket.catch_id = shared_models_catch.id
 LEFT JOIN shared_models_mission
     ON shared_models_sample.mission_id=shared_models_mission.id
 WHERE shared_models_mission.is_active="1"
