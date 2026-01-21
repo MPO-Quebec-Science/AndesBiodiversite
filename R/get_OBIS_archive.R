6989e0d155a01233496ded67216c086f0f282363
David Sean <david.sean@gmail.com>
2026-01-21 15:59:12 -0500

    forward quiet param

--- R/get_OBIS_archive.R
+++ R/get_OBIS_archive.R
@@ -6,7 +6,7 @@
 #' @param andes_db_connection a connection object to the ANDES database.
 #' @return A dataframe containing the OBIS data. Use `$occurrence` and `$event` to access the respective tables.  
 #' @export
-get_OBIS_archive <- function(andes_db_connection) {
+get_OBIS_archive <- function(andes_db_connection, ...) {
 
     andes_mission <- get_andes_mission(andes_db_connection)
     andes_sets <- get_andes_set(andes_db_connection, mission_id = andes_mission$id)
@@ -14,7 +14,7 @@ get_OBIS_archive <- function(andes_db_connection) {
 
     obis_occurrence_table <- NULL
     obis_event_table <- NULL
-    mission_event <- event_from_mission(andes_mission, parent = NULL)
+    mission_event <- event_from_mission(andes_mission, parent = NULL, ...)
 
     # events start as mission
     obis_event_table <- row_union(obis_event_table, mission_event)
@@ -22,7 +22,7 @@ get_OBIS_archive <- function(andes_db_connection) {
     # iterate through every Set in the mission
     for (i in seq_len(nrow(andes_sets))) {
         set <- andes_sets[i, ]
-        set_event <- event_from_fishing_set(set, parent_event = mission_event)
+        set_event <- event_from_fishing_set(set, parent_event = mission_event, ...)
         # add set event to obis_event_table
         obis_event_table <- row_union(obis_event_table, set_event)
 
@@ -30,7 +30,7 @@ get_OBIS_archive <- function(andes_db_connection) {
         andes_catches_in_this_set <- andes_catches[andes_catches$sample_number == set$sample_number,]
         for (j in seq_len(nrow(andes_catches_in_this_set))) {
             catch <- andes_catches_in_this_set[j, ]
-                occurrence <- occurrence_from_catch(catch, event = set_event)
+                occurrence <- occurrence_from_catch(catch, event = set_event, ...)
             obis_occurrence_table <- row_union(obis_occurrence_table, occurrence)
         }
     }
